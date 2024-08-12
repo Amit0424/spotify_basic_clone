0a6129cab4ffac2b017ff69f1c8fe1c1dfc64185
Amit <amitjat2406@gmail.com>
2024-08-13 03:11:25 +0530

    fix: implementation adjust according to favorite songs features

--- lib/service_locator.dart
+++ lib/service_locator.dart
@@ -3,9 +3,11 @@ import 'package:spotify_clone/data/repository/song/song_repository_impl.dart';
 import 'package:spotify_clone/data/sources/auth/auth_firebase_service.dart';
 import 'package:spotify_clone/data/sources/songs/song_firebase_service.dart';
 import 'package:spotify_clone/domain/repository/auth/auth.dart';
+import 'package:spotify_clone/domain/usecases/auth/get_user.dart';
 import 'package:spotify_clone/domain/usecases/auth/signin.dart';
 import 'package:spotify_clone/domain/usecases/auth/signup.dart';
 import 'package:spotify_clone/domain/usecases/song/add_or_remove_favorite_song.dart';
+import 'package:spotify_clone/domain/usecases/song/get_favorite_songs.dart';
 import 'package:spotify_clone/domain/usecases/song/get_news_songs.dart';
 import 'package:spotify_clone/domain/usecases/song/get_play_list.dart';
 
@@ -16,19 +18,18 @@ import 'domain/usecases/song/is_favorite_song.dart';
 final sl = GetIt.instance;
 
 Future<void> initializeDependencies() async {
-  sl.registerLazySingleton<AuthFirebaseService>(
-      () => AuthFirebaseServiceImpl());
+  sl.registerSingleton<AuthFirebaseService>(AuthFirebaseServiceImpl());
 
-  sl.registerLazySingleton<AuthRepository>(() => AuthRepositoryImpl());
-  sl.registerLazySingleton<SignupUseCase>(() => SignupUseCase());
-  sl.registerLazySingleton<SigninUseCase>(() => SigninUseCase());
-  sl.registerLazySingleton<SongFirebaseService>(
-      () => SongFirebaseServiceImpl());
-  sl.registerLazySingleton<SongRepository>(() => SongRepositoryImpl());
-  sl.registerLazySingleton<GetNewsSongsUseCase>(() => GetNewsSongsUseCase());
-  sl.registerLazySingleton<GetPlayListUseCase>(() => GetPlayListUseCase());
-  sl.registerLazySingleton<AddOrRemoveFavoriteSongUseCase>(
-      () => AddOrRemoveFavoriteSongUseCase());
-  sl.registerLazySingleton<IsFavoriteSongUseCase>(
-      () => IsFavoriteSongUseCase());
+  sl.registerSingleton<AuthRepository>(AuthRepositoryImpl());
+  sl.registerSingleton<SignupUseCase>(SignupUseCase());
+  sl.registerSingleton<SigninUseCase>(SigninUseCase());
+  sl.registerSingleton<SongFirebaseService>(SongFirebaseServiceImpl());
+  sl.registerSingleton<SongRepository>(SongRepositoryImpl());
+  sl.registerSingleton<GetNewsSongsUseCase>(GetNewsSongsUseCase());
+  sl.registerSingleton<GetPlayListUseCase>(GetPlayListUseCase());
+  sl.registerSingleton<AddOrRemoveFavoriteSongUseCase>(
+      AddOrRemoveFavoriteSongUseCase());
+  sl.registerSingleton<IsFavoriteSongUseCase>(IsFavoriteSongUseCase());
+  sl.registerSingleton<GetUserUseCase>(GetUserUseCase());
+  sl.registerSingleton<GetFavoriteSongsUseCase>(GetFavoriteSongsUseCase());
 }
